0bd193fceacb1ed97529a33a4492199886b72d88
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-12-03 16:59:12 +0530

    bug fixed

--- backend/services/validators.js
+++ backend/services/validators.js
@@ -1,4 +1,6 @@
-export const emailValidate = (email) => {
+const emailValidate = (email) => {
   let emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailRegExp.test(email);
 };
+
+module.exports = emailValidate;
--- backend/utils/authUtils.js
+++ backend/utils/authUtils.js
@@ -1,3 +1,5 @@
+const emailValidate = require("../services/validators");
+
 const userAuthRegisterUtils = async (
   firstname,
   lastname,
